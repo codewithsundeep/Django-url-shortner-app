9aa520c16482ab41247e94891f969ef1cabb4c83
Sundeep-sharma-neopaney <slowlymakesyouperfect@gmail.com>
2021-05-31 00:12:46 +0530

    second commit

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lj8l!xsk+n)xwxkiic98f5pksb6)g4!82kcrq7mp^1vv%llka0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sundeep-url-shortner.herokuapp.com']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,"static")
